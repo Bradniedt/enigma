fcc29aa470f0fd83ea9d172ca3a0f1d391049789
Brad Niedt <bradleyniedt@gmail.com>
2018-11-02 17:57:24 -0600

    adds code to pass get key test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,4 +3,12 @@ class Enigma
   def initialize
     @character_set = ('a'..'z').to_a << ' '
   end
+
+  def get_key
+    key = []
+    5.times do
+      key << rand(0..6)
+    end
+    key.join
+  end
 end
