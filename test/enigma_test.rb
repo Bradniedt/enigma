d65af4702f03d2e93a545d554dfdd44ce3a88b2d
Brad Niedt <bradleyniedt@gmail.com>
2018-11-02 17:27:50 -0600

    adds test for existence

---
+++ test/enigma_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/enigma'
+
+class EnigmaTest < Minitest::Test
+  def test_it_exists
+    enigma = Enigma.new
+    assert_instance_of Enigma, enigma 
+  end
+end
