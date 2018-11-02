fd0718fe1711a0a5f4efafa6089cf432af28a9ad
Brad Niedt <bradleyniedt@gmail.com>
2018-11-02 17:35:31 -0600

    adds test for character set. NOTE I will build out the entire encryption before breaking it out into separate classes and refactoring

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -6,6 +6,12 @@ require './lib/enigma'
 class EnigmaTest < Minitest::Test
   def test_it_exists
     enigma = Enigma.new
-    assert_instance_of Enigma, enigma 
+    assert_instance_of Enigma, enigma
+  end
+
+  def test_it_has_a_character_set
+    enigma = Enigma.new
+    expected = ('a'..'z').to_a << " " 
+    assert_equal expected, enigma.character_set
   end
 end
