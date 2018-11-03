9d8281007e87b15bef496b62a1616150351c2f8b
Brad Niedt <bradleyniedt@gmail.com>
2018-11-03 14:54:29 -0600

    removed unshift method from enigma and made lin 77 slightly shorter by changing encrypted letters variable to letters

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -68,22 +68,16 @@ class Enigma
     shifted_letters.join
   end
 
-  def unshift(shift_value, encrypted_letter)
-    encrypted_letter_index = @character_set.index(encrypted_letter)
-    rotated_characters = @character_set.rotate(-(shift_value))
-    true_letter = rotated_characters[encrypted_letter_index]
-  end
-
   def decrypt_message(encrypted_message, key, date)
     shift_values = shifter.final_shift_values(key, date)
-    encrypted_letters = encrypted_message.chars
+    letters = encrypted_message.chars
     unshifted_letters = []
-    (encrypted_letters.length).times do |i|
-      if @character_set.include?(encrypted_letters[i])
-        true_letter = unshift(shift_values.rotate(i)[0], encrypted_letters[i])
+    (letters.length).times do |i|
+      if @character_set.include?(letters[i])
+        true_letter = shifter.unshift(shift_values.rotate(i)[0], letters[i])
         unshifted_letters << true_letter
       else
-        unshifted_letters << encrypted_letters[i]
+        unshifted_letters << letters[i]
       end
     end
     unshifted_letters.join
