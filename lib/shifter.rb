3ccfbe5e0fe0832c9d5c0bcd9336c593f4043e1e
Brad Niedt <bradleyniedt@gmail.com>
2018-11-03 14:18:02 -0600

    adds code to pass get shift values test

--- lib/shifter.rb
+++ lib/shifter.rb
@@ -7,4 +7,13 @@ class Shifter
     4.times {|i| offsets << last_four[i].to_i}
     offsets
   end
+
+  def get_shift_values(key)
+    key_values = key.chars
+    key_numbers = []
+    (key_values.length - 1).times do |index|
+      key_numbers << ("#{key_values[index]}#{key_values[index + 1]}").to_i
+    end
+    key_numbers
+  end
 end
