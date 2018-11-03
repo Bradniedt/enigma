e9c48d5c0dd6557e0f96a293d9b1f10bbd64c9ba
Brad Niedt <bradleyniedt@gmail.com>
2018-11-03 16:10:13 -0600

    adds code to pass basic decryption test

--- lib/decrypter.rb
+++ lib/decrypter.rb
@@ -5,4 +5,18 @@ class Decrypter
     @shifter = Shifter.new
   end
 
+  def decrypt_message(encrypted_message, key, date)
+    shift_values = shifter.final_shift_values(key, date)
+    letters = encrypted_message.chars
+    unshifted_letters = []
+    (letters.length).times do |i|
+      if shifter.character_set.include?(letters[i])
+        true_letter = shifter.unshift(shift_values.rotate(i)[0], letters[i])
+        unshifted_letters << true_letter
+      else
+        unshifted_letters << letters[i]
+      end
+    end
+    unshifted_letters.join
+  end
 end
