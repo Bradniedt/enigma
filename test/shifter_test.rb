473877f76153e6061d2a813c4145691df9aed9d6
Brad Niedt <bradleyniedt@gmail.com>
2018-11-03 14:11:30 -0600

    adds require for shifter file

--- test/shifter_test.rb
+++ test/shifter_test.rb
@@ -3,11 +3,11 @@ SimpleCov.start
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'pry'
-require './lib/enigma'
+require './lib/shifter'
 
 class ShifterTest < Minitest::Test
   def test_it_exists
     shifter = Shifter.new
     assert_instance_of Shifter, shifter
-  end 
+  end
 end
