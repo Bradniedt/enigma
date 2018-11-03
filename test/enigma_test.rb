16219fb9d9e12acf48f817591181f1592611567d
Brad Niedt <bradleyniedt@gmail.com>
2018-11-02 19:00:52 -0600

    adds new test for final shift values and rewrites get shift values test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -31,7 +31,13 @@ class EnigmaTest < Minitest::Test
 
   def test_it_can_get_shift_values
     enigma = Enigma.new
+    expected = [2, 27, 71, 15]
+    assert_equal expected, enigma.get_shift_values('02715')
+  end
+
+  def test_it_can_get_final_shift_values
+    enigma = Enigma.new
     expected = [3, 27, 73, 20]
-    assert_equal expected, enigma.get_shift_values('02715', '040895')
+    assert_equal expected, enigma.final_shift_values('02715', '040895')
   end
 end
