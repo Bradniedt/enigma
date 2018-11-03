75765c7187d36181c46aa8385cf292904844dd10
Brad Niedt <bradleyniedt@gmail.com>
2018-11-03 14:13:15 -0600

    adds test for getting offset values

--- test/shifter_test.rb
+++ test/shifter_test.rb
@@ -10,4 +10,10 @@ class ShifterTest < Minitest::Test
     shifter = Shifter.new
     assert_instance_of Shifter, shifter
   end
+
+  def test_it_can_get_offset_values
+    shifter = Shifter.new
+    expected = [1, 0, 2, 5]
+    assert_equal expected, shifter.get_offset_values('040895')
+  end
 end
