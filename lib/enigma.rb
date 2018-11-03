c4935c0beef0a6870067eef6871f3ab550914358
Brad Niedt <bradleyniedt@gmail.com>
2018-11-02 18:30:03 -0600

    adds code to pass offset test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -12,5 +12,11 @@ class Enigma
     key.join
   end
 
-
+  def get_offset_values(date)
+    offsets = []
+    square = date.to_i * date.to_i
+    last_four = square.to_s.slice(-4..-1).chars
+    4.times {|i| offsets << last_four[i].to_i}
+    offsets
+  end
 end
