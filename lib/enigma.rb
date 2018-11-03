70c44ed8395849016f0424c1216500c160e3b4bf
Brad Niedt <bradleyniedt@gmail.com>
2018-11-03 15:30:02 -0600

    removing encryption methods from enigma to refactor enigma tests

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -24,48 +24,10 @@ class Enigma
     key.join
   end
 
-  def encrypt_message(message, key, date)
-    shift_values = shifter.final_shift_values(key, date)
-    letters = message.downcase.chars
-    shifted_letters = []
-    letters.each_index do |index|
-      letter = letters[index]
-      if @character_set.include?(letter)
-        if index % 4 == 0
-          shifted_letters << shifter.shift(shift_values[0], letter)
-        elsif index % 4 == 1
-          shifted_letters << shifter.shift(shift_values[1], letter)
-        elsif index % 4 == 2
-          shifted_letters << shifter.shift(shift_values[2], letter)
-        elsif index % 4 == 3
-          shifted_letters << shifter.shift(shift_values[3], letter)
-        end
-      else
-        shifted_letters << letter
-      end
-    end
-    shifted_letters.join
-  end
-
   def encrypt(message, key = get_key, date = get_todays_date)
     encryption_return = { encryption: encrypt_refactor(message, key, date),
                          key: key,
                          date: date }
-  end
-
-  def encrypt_refactor(message, key, date)
-    shift_values = shifter.final_shift_values(key, date)
-    letters = message.downcase.chars
-    shifted_letters = []
-    (letters.length).times do |i|
-      if @character_set.include?(letters[i])
-        new_letter = shifter.shift(shift_values.rotate(i)[0], letters[i])
-        shifted_letters << new_letter
-      else
-        shifted_letters << letters[i]
-      end
-    end
-    shifted_letters.join
   end
 
   def decrypt_message(encrypted_message, key, date)
