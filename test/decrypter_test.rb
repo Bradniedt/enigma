e80ec45c3171c1fa80b6b0be15dc028e9916a5ce
Brad Niedt <bradleyniedt@gmail.com>
2018-11-03 15:53:45 -0600

    adds test for accessing shifter

--- test/decrypter_test.rb
+++ test/decrypter_test.rb
@@ -9,5 +9,10 @@ class DecrypterTest < Minitest::Test
   def test_it_exists
     decrypter = Decrypter.new
     assert_instance_of Decrypter, decrypter
-  end 
+  end
+
+  def test_it_has_a_shifter
+    decrypter = Decrypter.new
+    assert_instance_of Shifter, decrypter.shifter
+  end
 end
