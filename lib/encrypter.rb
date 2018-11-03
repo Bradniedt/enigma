9fa05abfb0b6b0dd3ef366a59ea10c3d958b87b3
Brad Niedt <bradleyniedt@gmail.com>
2018-11-03 15:24:11 -0600

    adds code to pass basic encryption test

--- lib/encrypter.rb
+++ lib/encrypter.rb
@@ -5,4 +5,19 @@ class Encrypter
   def initialize
     @shifter = Shifter.new
   end
+
+  def encrypt_message(message, key, date)
+    shift_values = shifter.final_shift_values(key, date)
+    letters = message.downcase.chars
+    shifted_letters = []
+    (letters.length).times do |i|
+      if shifter.character_set.include?(letters[i])
+        new_letter = shifter.shift(shift_values.rotate(i)[0], letters[i])
+        shifted_letters << new_letter
+      else
+        shifted_letters << letters[i]
+      end
+    end
+    shifted_letters.join
+  end
 end
