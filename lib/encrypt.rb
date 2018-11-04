3a3df9fb544c42b100174c6f31778ef279023228
Brad Niedt <bradleyniedt@gmail.com>
2018-11-04 15:31:53 -0700

    clearing encrypt file to restart on CLI, clearly encountering issues with it.

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,9 +1,1 @@
-to_encrypt = ARGV.first
-encrypted = ARGV.second
-require './lib/enigma'
-require 'pry'
-enigma = Enigma.new
-message = open(to_encrypt, 'r+') { |f| f.read}
-encryption = open(encrypted, "r+")
-result = Enigma.encrypt(message, enigma.get_key, enigma.get_todays_date)
-encryption.write(result[:encryption])
+
