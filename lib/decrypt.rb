29424adfb18f6b59e2e5e851e57d70a6644d6234
Brad Niedt <bradleyniedt@gmail.com>
2018-11-05 12:50:18 -0700

    caught two typos - one in enigma random number generator and the other in the encrypt and decrypt files

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,7 +1,7 @@
 require './lib/enigma'
 encrypted_file, decrypted_file, key, date = ARGV
 enigma = Enigma.new
-message = open(encrypted_file, 'r+') { |f| f.read}
+message = open(encrypted_file, 'r') { |f| f.read}
 decryption = open(decrypted_file, "w")
 result = enigma.decrypt(message, key, date)
 decryption.write(result[:decryption])
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,7 +1,7 @@
 require './lib/enigma'
 message_file, encrypt_file = ARGV
 enigma = Enigma.new
-message = open(message_file, 'r+') { |f| f.read}
+message = open(message_file, 'r') { |f| f.read}
 encryption = open(encrypt_file, "w")
 result = enigma.encrypt(message, enigma.get_key, enigma.get_todays_date)
 encryption.write(result[:encryption])
